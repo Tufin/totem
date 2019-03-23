adf73ce9fbeed6480702c9f93f0006ab22e522a7
Effi <effoeffi@gmail.com>
2019-03-23 20:32:19 +0200

    "skipme" package created in order to test the "skip packages" feature

---
+++ skipme/invalid.go
@@ -0,0 +1,13 @@
+package skipme
+
+import (
+	"github.com/tufin/totem/analysis"
+	"github.com/tufin/totem/common"
+)
+
+// this file contains invalid import to analysis package
+// "skipme" package created in order to test the "skip packages" feature
+func InvalidImport() {
+
+	analysis.NewCrawler("", common.NewList(), common.NewList())
+}
