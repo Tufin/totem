acf589589636ab85a0b17ce8a4f47982e244d6e4
Effi <effoeffi@gmail.com>
2019-03-23 21:02:57 +0200

    test common get env

---
+++ common/env_test.go
@@ -0,0 +1,27 @@
+package common_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/tufin/totem/common"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	key   = "TEST_CHECK"
+	value = "ME"
+)
+
+func TestGetEnvOrExit(t *testing.T) {
+
+	require.NoError(t, os.Setenv(key, value))
+	require.Equal(t, value, common.GetEnvOrExit(key))
+}
+
+func TestGetEnv(t *testing.T) {
+
+	require.NoError(t, os.Setenv(key, value))
+	require.Equal(t, value, common.GetEnvOrExit(key))
+}
